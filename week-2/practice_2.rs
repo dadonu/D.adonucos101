52c7bb1c5142bdbc2fafaf56f2d40c238cf92ca7
dadonu <destiny.adonu@pau.edu.ng>
2026-09-02 20:46:45 +0100

    week-2

--- week-2/practice_2.rs
+++ week-2/practice_2.rs
@@ -2,7 +2,8 @@ fn main(){
  println!();// prints just a
 	println!("Hi {} good morning", "everyone,"); // prints Hi everyone, good morning
 println!("My name is Destiny, {} in PAN-ATLANTIC UNIVERSITY", "am studying Data science");
-/* prints My name is Destiny, am studying Data science in PAN-ATLANTIC UNIVERSITY*/
+/* prints My name is Destiny, am studying Data science in PAN-ATLANTIC UNIVERSITY
+And I tried combining project 2 and 3*/
 let f = "my annual school fees is";
 let d = "#8000000.00";
 println!("{},{}", f,d);
---
+++ week-2/project_2.rs
@@ -0,0 +1,15 @@
+fn main(){
+	// let Quantity=Q while Amount = A
+let _toshibaQ:f64 = 2.0;
+let _macQ:f64 = 1.0;
+let _hpQ:f64 = 3.0;
+let _dellQ:f64 = 3.0;
+let _acerQ:f64 = 1.0;
+let toshiba_A:f64 = 450_000.0;
+let mac_A:f64 = 1_500_000.0;
+let hp_A:f64 = 750_000.0;
+let dell_A:f64 = 2_850_000.0;
+let acer_A:f64 = 250_000.0;
+let _t = " toshibaQ * toshibaA ";
+println!("t");
+}
